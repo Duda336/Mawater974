946b3a65cd6eadabd8345ad9e44adfa371ad7deb
Duda336 <dudaduda336@gmail.com>
2025-02-05 21:27:12 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,38 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['eyhpjdnfeetmlayyxshx.supabase.co'],
-    unoptimized: true, // Disable Next.js image optimization
-  },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
-  experimental: {
-    forceSwcTransforms: true,
-    swcMinify: true,
+  images: {
+    unoptimized: true
   },
-  onError: () => {},
-  webpack: (config, { isServer }) => {
-    config.optimization = {
-      ...config.optimization,
-      checkWasmTypes: false,
-    }
-    return config
-  },
-  reactStrictMode: true,
-  // Vercel-specific configurations
-  vercel: {
-    skipFailedBuilds: true,
-  }
+  swcMinify: true
 };
 
 module.exports = nextConfig;
