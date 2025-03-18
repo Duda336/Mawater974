880e50b711d23433a053096bde742ffbd02b6949
Duda336 <dudaduda336@gmail.com>
2025-03-18 17:10:25 +0200

    next.config.js

--- next.config.js
+++ next.config.js
@@ -21,8 +21,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  },
-  output: 'standalone'
+  }
 };
 
 module.exports = nextConfig;
